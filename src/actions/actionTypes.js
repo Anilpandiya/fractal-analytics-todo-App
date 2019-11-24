5d8c4f94ee74a52aaf913360ff703e930c63a19a
ANIL PANDIYA <anilpandiya05@gmail.com>
2019-11-24 13:54:15 +0530

    added all require actions

---
+++ src/actions/actionTypes.js
@@ -0,0 +1,9 @@
+export const ADD_BUCKET = 'ADD_BUCKET';
+
+export const ADD_TASK = 'ADD_TASK';
+export const DELETE_TASK = 'DELETE_TASK';
+export const EDIT_TASK = 'EDIT_TASK';
+export const COMPLETE_TASK = 'COMPLETE_TASK';
+export const CANCEL_TASK = 'CANCEL_TASK';
+export const CANCEL_EDIT_TASK = 'CANCEL_EDIT_TASK';
+export const SELECT_EDIT_TASK = 'SELECT_EDIT_TASK';
